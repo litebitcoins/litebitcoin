093dfb2d2910bffa7a5a517619f25a16d4770dac
mraksoll <mraksoll@gmail.com>
2017-07-26 23:29:46 +0300

    delete trash

--- src/main.cpp
+++ src/main.cpp
@@ -1139,22 +1139,7 @@ unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBl
 
     // Only change once per interval
     if ((pindexLast->nHeight+1) % nInterval != 0)
-		
     {
-		{
-            // If the new block's timestamp is more than 2* 10 minutes
-            // then allow mining of a min-difficulty block.
-            if (pblock->nTime > pindexLast->nTime + nTargetSpacing*2)
-                return nProofOfWorkLimit;
-            else
-            {
-                // Return the last non-special-min-difficulty-rules-block
-                const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nProofOfWorkLimit)
-                    pindex = pindex->pprev;
-                return pindex->nBits;
-            }
-        }
         // Special difficulty rule for testnet:
         if (fTestNet)
         {
