c46f9bcec5d0e17889514459cd7b8dd129b2bbb1
litebitcoins <litebitcoins@gmail.com>
2017-08-11 19:18:41 +0300

    fix wrong value on Chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -80,8 +80,8 @@ public:
         consensus.nPowTargetSpacing = 5 * 60;
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
-        consensus.nRuleChangeActivationThreshold = 8; // 75% of 8064
-        consensus.nMinerConfirmationWindow = 6; // nPowTargetTimespan / nPowTargetSpacing * 4
+        consensus.nRuleChangeActivationThreshold = 6; // 75% of 8064
+        consensus.nMinerConfirmationWindow = 8; // nPowTargetTimespan / nPowTargetSpacing * 4
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
